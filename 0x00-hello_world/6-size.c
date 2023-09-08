7a69f22e63930c48fa7083a3f0614efc583602fa
IamElistone <oguttuelly@gmail.com>
2023-09-07 21:14:26 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+/**
+ * main -size of many various types
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+printf("Size of a char: %lu byte(s)", sizeof(char));
+printf("size of int: %lu byte(s)", sizeof(int));
+printf("size of a long int: %lu byte(s)", sizeof(long int));
+printf("size of a long long int: %lu byte(s)", sizeof(long long int));
+printf("size of a float: %lu bytes(s)", sizeof(float));
+return (0);
+}
